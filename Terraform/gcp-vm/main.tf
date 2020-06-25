4dd678acc4489ee6ba1b1be82773fc46375a0e91
Ahmed <ahmedatef844.218@gmail.com>
2020-06-25 14:19:24 +0200

    add docs

--- Terraform/gcp-vm/main.tf
+++ Terraform/gcp-vm/main.tf
@@ -21,7 +21,8 @@ resource "google_compute_instance" "default" {
 
  boot_disk {
    initialize_params {
-     image = "debian-cloud/debian-9"
+  // image = "debian-cloud/debian-9"
+     image = "ubuntu-1804-lts"
    }
  }
 
